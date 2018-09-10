2d776600e33b870a6f8f52abfe9a1b3573f41614
Matheus <matheus.r.n@hotmail.com>
2018-09-10 18:50:13 -0300

    archs moviment

---
+++ Assets/_Project/Scripts/ControlRespawnMoviment.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlRespawnMoviment : MonoBehaviour {
+
+    [SerializeField] private Animator[] animator;
+    private float timer;
+    private float cooldown;
+    private int archId;
+	// Use this for initialization
+	void Start () {
+        timer = 0;
+        cooldown = 10.5f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (archId < animator.Length) {
+
+            timer += Time.deltaTime;
+
+            if (cooldown <= timer) {
+
+                animator[archId].SetBool("run", true);
+
+                archId++;
+                timer = 0;
+                cooldown *= 2;
+            }
+        }
+	}
+}
